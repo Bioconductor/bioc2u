88f2218f6c1c7d307872380fa15c0ff1a06195dc
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-09-29 15:03:11 -0400

    Add biocmanager

--- apt_setup.sh
+++ apt_setup.sh
@@ -4,3 +4,5 @@ curl -O https://raw.githubusercontent.com/eddelbuettel/r2u/master/inst/scripts/a
 bash add_cranapt_$UBUNTU_CODENAME.sh
 rm add_cranapt_$UBUNTU_CODENAME.sh
 echo "deb [trusted=yes] https://js2.jetstream-cloud.org:8001/swift/v1/bioc2u/ $UBUNTU_CODENAME release" | tee -a /etc/apt/sources.list.d/bioc2u.list > /dev/null
+apt update -qq
+DEBIAN_FRONTEND=noninteractive apt install -y r-cran-biocmanager
