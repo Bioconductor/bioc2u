95a4d62741ad4a02a9b3450cbf158420e7842ba3
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-09-29 14:59:07 -0400

    Script for apt setup

---
+++ apt_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export UBUNTU_CODENAME=$(grep '^VERSION_CODENAME=' /etc/os-release | cut -d= -f2)
+curl -O https://raw.githubusercontent.com/eddelbuettel/r2u/master/inst/scripts/add_cranapt_$UBUNTU_CODENAME.sh
+bash add_cranapt_$UBUNTU_CODENAME.sh
+rm add_cranapt_$UBUNTU_CODENAME.sh
+echo "deb [trusted=yes] https://js2.jetstream-cloud.org:8001/swift/v1/bioc2u/ $UBUNTU_CODENAME release" | tee -a /etc/apt/sources.list.d/bioc2u.list > /dev/null
