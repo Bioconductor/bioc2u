b52d53b66533653b5f1cc62928a43bf12475b051
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-10-19 16:49:41 -0400

    Add key and use OSN signed repo

--- apt_setup.sh
+++ apt_setup.sh
@@ -5,6 +5,10 @@ apt install -y --no-install-recommends curl ca-certificates
 curl -O https://raw.githubusercontent.com/eddelbuettel/r2u/master/inst/scripts/add_cranapt_$UBUNTU_CODENAME.sh
 bash add_cranapt_$UBUNTU_CODENAME.sh
 rm add_cranapt_$UBUNTU_CODENAME.sh
-echo "deb [trusted=yes] https://js2.jetstream-cloud.org:8001/swift/v1/bioc2u/ $UBUNTU_CODENAME release" | tee -a /etc/apt/sources.list.d/bioc2u.list > /dev/null
+curl -o /etc/apt/trusted.gpg.d/bioc2u_key.asc https://mghp.osn.xsede.org/bir190004-bucket01/bioc2u/bioc2u_key.asc
+echo "deb https://mghp.osn.xsede.org/bir190004-bucket01/bioc2u/ $UBUNTU_CODENAME release" | tee -a /etc/apt/sources.list.d/bioc2u.list > /dev/null
+echo "Package: *" > /etc/apt/preferences.d/prefbioc2u
+echo "Pin: origin mghp.osn.xsede.org" >> /etc/apt/preferences.d/prefbioc2u
+echo "Pin-Priority: 800"  >> /etc/apt/preferences.d/prefbioc2u
 apt update -qq
 DEBIAN_FRONTEND=noninteractive apt install -y r-base-core r-cran-biocmanager r-cran-bspm
