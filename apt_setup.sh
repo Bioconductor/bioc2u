e257be6ad00f57e9057908c11cc21742533283fc
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2024-05-06 14:16:04 -0400

    Bump versions to 3.19

--- apt_setup.sh
+++ apt_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BIOC_VERSION=${1:-"3.18"}
+BIOC_VERSION=${1:-"3.19"}
 export UBUNTU_CODENAME=$(grep '^VERSION_CODENAME=' /etc/os-release | cut -d= -f2)
 apt update -qq
 apt install -y --no-install-recommends curl ca-certificates
